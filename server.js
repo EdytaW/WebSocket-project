586df07bf8bfe66996f7784416ee04167dd6b79e
Edyta Wojdala <edytawoj095@gmail.com>
2020-12-01 16:01:24 +0100

    Add path

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
-
+const socket = require('socket.io');
 const app = express();
+
 
 const messages = [];
 app.use(express.static(path.join(__dirname, '/client')));
@@ -9,6 +10,14 @@ app.get('*', (req, res) => {
     res.render(path.join(__dirname, '/client/index.html'));
   });
 
-app.listen(process.env.PORT || 8000, () => {
+const server = app.listen(process.env.PORT || 8000, () => {
     console.log('Server is running on port: 8000');
   });
+const io = socket(server); 
+
+io.on('connection', (socket) => {
+  console.log('New client! Its id – ' + socket.id);
+  socket.on('message', () => { console.log('Oh, I\'ve got something from ' + socket.id) });
+  console.log('I\'ve added a listener on message event \n');
+});
+
